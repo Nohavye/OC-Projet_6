66eab3a22433245317b04769c3f8b10a07200c30
Nohavye <noha.poncelet@gmail.com>
2023-02-24 15:19:30 +0100

    Correction ESLint

--- scripts/api/dataManager.js
+++ scripts/api/dataManager.js
@@ -1,54 +1,53 @@
-/** 
+/**
  * Gestion de données JSON. */
 export class dataManager {
-    static #data
+  static #data
 
-    /**
+  /**
      * Charger des données depuis un fichier JSON.
      * @param {string} url Chemin du fichier JSON.
      */
-    static async loadData(url) {
-        this.#data = await fetch(url)
-            .then(answer => answer.json())
-            .catch(error => console.error(error))
-    }
+  static async loadData (url) {
+    this.#data = await fetch(url)
+      .then(answer => answer.json())
+      .catch(error => console.error(error))
+  }
 
-    /**
+  /**
      * Récupérer les données d'une section.
      * @param {string} section Section de fichier JSON.
      * @returns Retourne les données compris dans la section.
      */
-    static getData(section) {
-        try {
-            return this.#data[`${section}`]
-        } catch(error) {
-            console.error(error)
-        }
+  static getData (section) {
+    try {
+      return this.#data[`${section}`]
+    } catch (error) {
+      console.error(error)
     }
+  }
 
-    /**
+  /**
      * Récupérer un objet en fonction de la valeur d'une de ses propriété.
      * @param {string} section Section de fichier JSON visée.
      * @param {string} property Propriété visée.
      * @param {any} value Valeur de cette propriété.
      * @returns Un tableau contenant tout les objets ayant la propriété visée à la valeur spécifiée.
      */
-    static search(section, property, value) {
-        const results = []
+  static search (section, property, value) {
+    const results = []
 
-        try {
-            const data = this.#data[`${section}`]
-            
-            for(let element of data) {
-                if(element[`${property}`] === value) {
-                    results.push(element)
-                }
-            }
+    try {
+      const data = this.#data[`${section}`]
 
-            return results
-
-        } catch(error) {
-            console.error(error)
+      for (const element of data) {
+        if (element[`${property}`] === value) {
+          results.push(element)
         }
+      }
+
+      return results
+    } catch (error) {
+      console.error(error)
     }
+  }
 }
